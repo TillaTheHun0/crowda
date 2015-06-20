a9455ea96a98e4ef4e6aae4dd481eb059ff675f8
Tyler Hall <0tillathehun0@gmail.com>
2015-06-19 21:23:50 -0400

    initial commit of blank project

---
+++ crowda/crowda/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Crowda
+//
+//  Created by Tyler Hall on 6/19/15.
+//  Copyright (c) 2015 Tyler Hall. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
